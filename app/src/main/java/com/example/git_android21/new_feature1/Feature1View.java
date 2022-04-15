8545bafc9602ba63f6d5f63d64ae6c9ce932a84c
Duy Le <lehongduy.bk@gmail.com>
2022-04-15 19:54:06 +0700

    update Feature1View

--- app/src/main/java/com/example/git_android21/new_feature1/Feature1View.java
+++ app/src/main/java/com/example/git_android21/new_feature1/Feature1View.java
@@ -1,4 +1,32 @@
 package com.example.git_android21.new_feature1;
 
 public class Feature1View {
+
+    String id;
+    int width;
+    int height;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
