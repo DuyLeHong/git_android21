e86be5950cc0c1608f24ee6e533a85eb56008020
Duy Le <lehongduy.bk@gmail.com>
2022-04-15 19:27:45 +0700

    update Feature1Model

--- app/src/main/java/com/example/git_android21/new_feature1/Feature1Model.java
+++ app/src/main/java/com/example/git_android21/new_feature1/Feature1Model.java
@@ -1,4 +1,23 @@
 package com.example.git_android21.new_feature1;
 
 public class Feature1Model {
+
+    private String sName = "";
+    private int age = 0;
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
