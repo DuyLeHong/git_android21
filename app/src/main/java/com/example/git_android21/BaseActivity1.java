95b0a7fd592e623d118985daad44e2fee66b6b2a
Duy Le <lehongduy.bk@gmail.com>
2022-04-15 19:44:14 +0700

    fix bug in BaseActivity1

--- app/src/main/java/com/example/git_android21/BaseActivity1.java
+++ app/src/main/java/com/example/git_android21/BaseActivity1.java
@@ -3,12 +3,17 @@ package com.example.git_android21;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class BaseActivity1 extends AppCompatActivity {
+public class BaseActivity1 extends AppCompatActivity { // fix bug in BaseActivity1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base1);
+
+        //Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+        Log.d("BaseActivity1", "fix some bug");
     }
 }
